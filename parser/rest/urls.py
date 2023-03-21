f3910a0bee1fffeff1c2308bddbe318c5768bd89
Zahid Khan <63726443+zahikhan@users.noreply.github.com>
2023-03-21 20:10:02 +0530

    Remove trailing forward slash.
    Do not use trailing forward slash (/) in URIs.

--- parser/rest/urls.py
+++ parser/rest/urls.py
@@ -3,7 +3,7 @@ from parser.rest import views
 
 
 urlpatterns = [
-    path('files/csv/upload/', views.UploadCsvFileAPIView.as_view(), name='upload_csv_file'),
-    path('files/csv/<str:file_name>/parse/', views.ParseCsvFileToDatabaseAPIView.as_view(), name='parse_csv_file'),
-    path('files/csv/<str:file_name>/', views.CsvDatabaseFileAPIView.as_view(), name='csv_file')
+    path('files/csv/upload', views.UploadCsvFileAPIView.as_view(), name='upload_csv_file'),
+    path('files/csv/<str:file_name>/parse', views.ParseCsvFileToDatabaseAPIView.as_view(), name='parse_csv_file'),
+    path('files/csv/<str:file_name>', views.CsvDatabaseFileAPIView.as_view(), name='csv_file')
 ]
